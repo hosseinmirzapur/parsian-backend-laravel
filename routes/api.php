1ccc1da421d775d33a7152884e2e6f77bfb189fa
Hossein Mirzapur <63141950+hosseinmirzapur@users.noreply.github.com>
2023-09-14 04:36:17 +0330

    created routes for user, admin and profile

--- routes/api.php
+++ routes/api.php
@@ -16,10 +16,29 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Order
-Route::resource('/order', OrderController::class);
+Route::resource('/order', OrderController::class)->middleware('auth:sanctum');
 
 // Order Item
-Route::resource('/order-item', OrderItemController::class)->except('index');
+Route::resource('/order-item', OrderItemController::class)->except('index')->middleware('auth:sanctum');
+
+// User
+Route::prefix('/user')->group(function () {
+    // User Profile
+    Route::prefix('/profile')->group(function () {
+        Route::post('/alter');
+    })->middleware('auth:sanctum');
+
+    // Other User Routes
+    Route::post('/login');
+    Route::post('/register');
+    Route::get('/')->middleware('auth:sanctum');
+
+});
+
+// Admin
+Route::prefix('/admin')->group(function () {
+    Route::post('/login');
+});
 
 Route::get('/test', function () {
     return successResponse([
